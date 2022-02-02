c6fb269a9d260415a00f5063144e178bcd7c6009
Victoria Orlova <victoria1orlova@gmail.com>
2022-02-02 15:50:27 +0200

    I'm back, Hi!

---
+++ jsBook/main.js
@@ -0,0 +1,9 @@
+console.log("Well hi, I'm Vicky");
+var drawKitty = function (howManyTimes)
+{
+    for (var i = 0; i < howManyTimes; i++)
+    {
+        console.log(i + "=*w*=")
+    }
+};
+drawKitty(12);
