fd6b8eea4b9e09dc541968a1bcc4f3de970e6c72
Victoria Orlova <victoria1orlova@gmail.com>
2022-02-16 17:39:10 +0200

    array.js added arrays

--- jsBook/chapters/chapter_03/array.js
+++ jsBook/chapters/chapter_03/array.js
@@ -19,3 +19,20 @@ var dinosaur = ["T-Rex",
                 "Diplodocus",
                 "Compsognathus"
 ]; // that's array too
+
+var dinosaurus = [];
+dinosaurus[0] = "T-Rex";
+dinosaurus[2] = "Velocirator";
+dinosaurus[2] = "Stegosaurus";
+dinosaurus[3] = "Tricerators";
+dinosaurus[4] = "Branchiosaurus";
+dinosaurus[5] = "Pteranodon";
+dinosaurus[6] = "Apatosaurus";
+dinosaurus[7] = "Diplodocus";
+dinosaurus[8] = "Compsognathus";
+console.log(dinosaurus[6]);
+
+var lessons = [];
+var aditionalTasks = [];
+var myWork = [[lessons], [aditionalTasks]];
+
